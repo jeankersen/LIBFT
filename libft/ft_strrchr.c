df004ec21ff17800fe9a631f926eb1e98c50db64
jeankersen <61942315+jeankersen@users.noreply.github.com>
2023-01-11 14:34:17 -0500

    Add files via upload

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvillefr <jvillefr@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/30 14:00:16 by jvillefr          #+#    #+#             */
+/*   Updated: 2023/01/11 14:31:50 by jvillefr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+
+	size_t	i;
+
+
+	i = ft_strlen(s);
+	while (s[i] >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i--;
+	}
+	if (s[i] == (char)c)
+		return ((char *)(s + i));
+	return (NULL);
+}
